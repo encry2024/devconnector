0835ab40ba45d430478f2a0769c7db9b53b85e78
Encrypt <christan.jake2025@yahoo.com>
2020-04-19 17:59:55 +0800

    Initial commit.

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://sydcipher:' + encodeURIComponent('EF7X5xkw/HTs#a!') + '@cluster0-5svea.mongodb.net/test?retryWrites=true&w=majority'
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+// Express
+const express = require('express');
+const app = express();
+// Mongoose
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+// Port
+const port = process.env.PORT || 5000;
+// Routes
+const users = require('./routes/api/users'),
+    profile = require('./routes/api/profile'),
+    posts = require('./routes/api/posts');
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
