e90bef1e96de5ce5eb895d79f6593e2f123c96a9
Encrypt <christan.jake2025@yahoo.com>
2020-04-20 15:21:05 +0800

    Added secretOrKey for security.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://sydcipher:' + encodeURIComponent('EF7X5xkw/HTs#a!') + '@cluster0-5svea.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://sydcipher:' + encodeURIComponent('EF7X5xkw/HTs#a!') + '@cluster0-5svea.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
 };
