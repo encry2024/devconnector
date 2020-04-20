273248828af8c18afe727f1f1a378df06e56b954
Encrypt <christan.jake2025@yahoo.com>
2020-04-20 15:23:57 +0800

    Added additional routes and validations.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 // Load user model
 const User = require('../../models/User');
+
+// Load User Validation Input
+const validateRegistrationInput = require('../../validation/register');
 
 // @route   GET     /api/users/test
 // @desc    Tests   users route
@@ -26,6 +32,12 @@ router.post('/register',
         req,
         res
     ) => {
+        const { errors, isValid } = validateRegistrationInput(req.body);
+
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
         User.findOne(
             {
                 email: req.body.email
@@ -61,6 +73,76 @@ router.post('/register',
                 });
             }
         })
+    }
+);
+
+// @route   POST    /api/users/login
+// @desc    Login   User / Returning JWT Token
+// @access  Public
+router.post('/login',
+    (
+        req,
+        res
+    ) => {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        User.findOne({email})
+            .then(user => {
+                if (!user) {
+                    return res.status(404).json({email: 'User not found'});
+                }
+
+                bcrypt.compare(password, user.password)
+                    .then(isMatch => {
+                        if (isMatch) {
+                            const payload = {
+                                id: user.id,
+                                name: user.name,
+                                avatar: user.avatar
+                            };
+
+                            jwt.sign(
+                                payload,
+                                keys.secretOrKey,
+                                { expiresIn: 3600 },
+                                (err, token) => {
+                                    res.json({
+                                        success: true,
+                                        token: 'Bearer: ' + token
+                                    })
+                                });
+                        } else {
+                            return res.status(400)
+                                .json({password: 'E-mail or Password is incorrect'});
+                        }
+                    })
+            })
+    }
+);
+
+// @route   GET    /api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+    '/current',
+    passport.authenticate
+    (
+        'jwt',
+        {
+            session: false
+        }
+    ), (
+        req,
+        res
+    ) => {
+        res.json(
+            {
+                id: req.user.id,
+                name: req.user.name,
+                email: req.user.email
+            }
+        );
     }
 );
 
